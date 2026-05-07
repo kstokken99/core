6945f1e2017d171ec079c9d6fe6b941e7792c83a
Artem Ivanov <a.ivanov@yavlenie.pro>
2026-05-07 20:11:15 +0500

    feat: 📦 BundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,7 +1,8 @@
-import webpack from "webpack";
-import HTMLWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import { BuildOptions } from "./types";
+import webpack from 'webpack'
+import HTMLWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import { BuildOptions } from './types'
 
 export const buildPlugins = ({
   paths,
@@ -12,11 +13,14 @@ export const buildPlugins = ({
     template: paths.html,
   }),
   new MiniCssExtractPlugin({
-    filename: "css/[name].[contenthash].css",
-    chunkFilename: "css/[name].[contenthash].css",
+    filename: 'css/[name].[contenthash].css',
+    chunkFilename: 'css/[name].[contenthash].css',
   }),
   new webpack.DefinePlugin({
     __IS_DEV__: JSON.stringify(isDev),
   }),
   new webpack.HotModuleReplacementPlugin(),
-];
+  new BundleAnalyzerPlugin({
+    openAnalyzer: false,
+  }),
+]
